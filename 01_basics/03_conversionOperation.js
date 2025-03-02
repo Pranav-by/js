83f362fde86a8cb510be70eff0b0ca73107f0d46
Pranav-by <155034053+Pranav-by@users.noreply.github.com>
2025-03-02 04:04:50 +0000

    conversionOperator

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,26 +1,51 @@
-let score = "33abs"
+// let score = "33abs"
 
-console.log(typeof score)
-console.log(typeof(score))
+// console.log(typeof score)
+// console.log(typeof(score))
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
 
-// "33" => 33
-// "33abc" => NaN
-// true => 1; false => 0
+// // "33" => 33
+// // "33abc" => NaN
+// // true => 1; false => 0
 
-let isLoggedIn = 1
-let booleanIsLoggedIn = Boolean(isLoggedIn)
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
 
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
-// "1" => true ; 0 => false
-// "" => false
-// "hello" => true
+// // "1" => true ; 0 => false
+// // "" => false
+// // "hello" => true
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber)
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber)
+
+
+
+// ***************** Operations *******************
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+let str1 = "hello"
+let str2 = "world"
+
+let st3 = str1 + str2
+
+
+console.log("1" + 2)
+console.log(1 +"2")
+console.log("1" +2 +2)
+console.log(1 + 2 + "2")
+
+
+console.log(true);
+console.log(+"")
+
+let num1,num2 , num3
+num1 = num2 = num3 = 2+2
